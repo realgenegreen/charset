8e21224b98916334f457fd98b9b159c662ce81d0
realgenegreen <realgenegreen@gmail.com>
2023-10-30 18:07:02 +0300

    2

--- charset.py
+++ charset.py
@@ -9,25 +9,16 @@ def charset(value: int, numbers= False, symbols= False, letterless= False) -> st
         // symbols is the presence of symbols in line
         // letterless remove letters
     '''
-    chset = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm'
-    nuset = '1234567890'
-    syset = '-:;!?.,'
+    chset = ''
+    nuset = '1234567890' * 4
+    syset = '-:;!?.,' * 6
 
-    if letterless is True:
-        chset = ''
+    if not letterless:
+        chset = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm'
+    if numbers:
+        chset += nuset
+    if symbols:
+        chset += syset
 
-    def _chlist():
-        set_list = ''
-        if numbers is True and symbols is False:
-            set_list = chset+(nuset*4)
-        if symbols is True and numbers is False:
-            set_list = chset+(syset*6)
-        if numbers is True and symbols is True:
-            set_list = chset+(nuset*2)+(syset*3)
-        if numbers is False and symbols is False:
-            set_list = chset
-        return set_list
-
-    li = _chlist()
-    pwd = ''.join([random.choice(li) for x in range(value)])
-    return pwd
+    line = ''.join([random.choice(chset) for x in range(value)]) if chset != '' else ''
+    return line
